9bd8daaa81eb7950b161d278baeeec3f1da3937f
Niklas Lochschmidt <nlochschmidt@gmail.com>
2019-06-30 01:32:37 +0200

    Implement simple monkey program lexer

--- src/commonMain/kotlin/lexer/Lexer.kt
+++ src/commonMain/kotlin/lexer/Lexer.kt
@@ -1,6 +1,7 @@
 package lexer
 
 import token.Token
+import token.TokenType
 import token.TokenType.*
 
 class Lexer(val input: String) {
@@ -14,6 +15,7 @@ class Lexer(val input: String) {
   }
   
   fun nextToken(): Token {
+    skipWhitespace()
     val token = when (currentChar) {
       '=' -> Token(ASSIGN, "$currentChar")
       ';' -> Token(SEMICOLON, "$currentChar")
@@ -23,10 +25,43 @@ class Lexer(val input: String) {
       '+' -> Token(PLUS, "$currentChar")
       '{' -> Token(LBRACE, "$currentChar")
       '}' -> Token(RBRACE, "$currentChar")
-      else -> Token(EOF, "")
+      NULL -> Token(EOF, "")
+      else -> {
+        if (currentChar.isLetter()) {
+          val identifier = readIdentifier()
+          return lookupIdentifierToken(identifier)
+        } else if (currentChar.isDigit()) {
+          return Token(INT, readNumber())
+        } else {
+          Token(ILLEGAL, "$currentChar")
+        }
+      }
     }
     readChar()
     return token
+  }
+
+  private fun readIdentifier(): String {
+    val startPosition = position
+    while(currentChar.isLetter() || currentChar == '_') {
+      readChar()
+    }
+    return input.substring(startPosition, position)
+  }
+
+  private fun lookupIdentifierToken(identifier: String): Token {
+    return Token(
+      type = KEYWORDS.getOrElse(identifier) { IDENT }, 
+      literal = identifier
+    )
+  }
+
+  private fun readNumber(): String {
+    val startPosition = position
+    while (currentChar.isDigit()) {
+      readChar()
+    }
+    return input.substring(startPosition, position)
   }
 
   private fun readChar() {
@@ -35,11 +70,22 @@ class Lexer(val input: String) {
     } else {
       currentChar = input[readPosition]
     }
-    position += readPosition
+    position = readPosition
     readPosition += 1
+  }
+
+  private fun skipWhitespace() {
+    while (currentChar.isWhitespace() && currentChar != NULL) {
+      readChar()
+    }
   }
 
   companion object {
     const val NULL: Char = '\u0000'
+
+    val KEYWORDS : Map<String, TokenType>  = mapOf(
+      "fn" to FUNCTION,
+      "let" to LET
+    )
   }
 }
--- src/commonTest/kotlin/lexer/LexerTest.kt
+++ src/commonTest/kotlin/lexer/LexerTest.kt
@@ -28,7 +28,67 @@ class LexerTest {
     val lexer = Lexer(input)
 
     expectedTokens.forEach {
-      assertEquals(lexer.nextToken(), it)
+      assertEquals(it, lexer.nextToken())
+    }
+  }
+
+  @Test
+  fun `nextToken on simplified monkey program`() {
+    val input = """
+      let five = 5;
+      let ten = 10;
+
+      let add = fn(x, y) {
+        x + y;
+      };
+
+      let result = add(five, ten);
+      """.trimIndent()
+
+    val expectedTokens = listOf(
+      Token(LET, "let"),
+      Token(IDENT, "five"),
+      Token(ASSIGN, "="),
+      Token(INT, "5"),
+      Token(SEMICOLON, ";"),
+      Token(LET, "let"),
+      Token(IDENT, "ten"),
+      Token(ASSIGN, "="),
+      Token(INT, "10"),
+      Token(SEMICOLON, ";"),
+      Token(LET, "let"),
+      Token(IDENT, "add"),
+      Token(ASSIGN, "="),
+      Token(FUNCTION, "fn"),
+      Token(LPAREN, "("),
+      Token(IDENT, "x"),
+      Token(COMMA, ","),
+      Token(IDENT, "y"),
+      Token(RPAREN, ")"),
+      Token(LBRACE, "{"),
+      Token(IDENT, "x"),
+      Token(PLUS, "+"),
+      Token(IDENT, "y"),
+      Token(SEMICOLON, ";"),
+      Token(RBRACE, "}"),
+      Token(SEMICOLON, ";"),
+      Token(LET, "let"),
+      Token(IDENT, "result"),
+      Token(ASSIGN, "="),
+      Token(IDENT, "add"),
+      Token(LPAREN, "("),
+      Token(IDENT, "five"),
+      Token(COMMA, ","),
+      Token(IDENT, "ten"),
+      Token(RPAREN, ")"),
+      Token(SEMICOLON, ";"),
+      Token(EOF, "")
+    )
+
+    val lexer = Lexer(input)
+
+    expectedTokens.forEach {
+      assertEquals(it, lexer.nextToken())
     }
   }
 }
