9167cb3ba4824017f7b8d70effe3218bba8ffa17
Niklas Lochschmidt <nlochschmidt@gmail.com>
2019-06-30 13:57:09 +0200

    Start working on the AST

---
+++ src/commonMain/kotlin/ast/AST.kt
@@ -0,0 +1,33 @@
+package kotlin.ast
+
+import token.Token
+
+interface Node {
+  val literal: String
+}
+
+interface Statement : Node
+
+interface Expression : Node
+
+data class Program(val statements: List<Statement>) : Node {
+  override val literal: String
+    get() = statements.firstOrNull()?.literal ?: ""
+}
+
+data class LetStatement(
+  val token: Token,
+  val name: Identifier,
+  val value: Expression
+) : Statement {
+  override val literal: String
+    get() = token.literal
+}
+
+data class Identifier(
+  val token: Token,
+  val value: String
+): Expression {
+  override val literal: String
+    get() = token.literal
+}
