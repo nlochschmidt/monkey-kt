948fe6951907b5dae2a1a09263bef231b9a296c6
Niklas Lochschmidt <nlochschmidt@gmail.com>
2019-07-14 13:55:06 +0200

    Improve REPL

--- src/commonMain/kotlin/repl/REPL.kt
+++ src/commonMain/kotlin/repl/REPL.kt
@@ -1,6 +1,7 @@
 package repl
 
 import lexer.Lexer
+import parser.Parser
 import token.TokenType.EOF
 
 class REPL {
@@ -10,16 +11,40 @@ class REPL {
       write(PROMPT)
       val line = readLine().takeIf { it != "" } ?: return
       val lexer = Lexer(line)
-      var token = lexer.nextToken()
-      while (token.type != EOF) {
-        write(token.toString())
-        write("\n")
-        token = lexer.nextToken()
+      val parser = Parser(lexer)
+      val program = parser.parseProgram()
+      if (parser.errors.isNotEmpty()) {
+        printParseErrors(write, parser.errors)
+        continue
       }
+      write(program.toString())
+      write("\n")
+    }
+  }
+
+  private fun printParseErrors(write: (String) -> Unit, errors: List<String>) {
+    write(MONKEY_FACE)
+    write("Woops! We ran into some monkey business here!\n")
+    write(" parser errors:\n")
+    errors.forEach { error ->
+      write("\t$error\n")
     }
   }
 
   companion object {
     const val PROMPT = ">> "
+
+    const val MONKEY_FACE = """            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-""${'"'}${'"'}${'"'}${'"'}${'"'}-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+    """
   }
 }
